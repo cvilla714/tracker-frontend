4ba42fb3f01facbf373690fc5348c1d6e05ea2ae
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-18 02:34:34 -0400

    deleted word anchor and formatted with prettier

--- api/server.js
+++ api/server.js
@@ -1,15 +1,15 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
-app.get('/'.anchor(req,res)=>{
-    res.send('Hello from index route')
+app.get('/', (req, res) => {
+  res.send('Hello from index route');
 });
 
-app.get('/protected',(req,res)=>{
-    res.send('Hello from protected route')
-})
+app.get('/protected', (req, res) => {
+  res.send('Hello from protected route');
+});
 
-app.listen(4000,()=>console.log('Server running on port 4000'))
+app.listen(4000, () => console.log('Server running on port 4000'));
