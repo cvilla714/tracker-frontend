5bb769cdd97f58a93d944884e52bb4759879c63e
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-18 02:11:57 -0400

    added Auth0Provider to wrap the entire app and have access to all core funtionalities for authentication

--- src/index.js
+++ src/index.js
@@ -4,18 +4,19 @@ import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Auth0Provider
+        domain="dev-v88tfgqc.us.auth0.com"
+        clientId="j8AoqGO5QXuocjqV9vqywdhBg3iWC2C8"
+        redirectUri={window.location.origin}
+      >
+        <App />
+      </Auth0Provider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
