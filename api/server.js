2e1d2dfca89174d1cacecb3ecdff06fb3d888854
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-18 04:26:16 -0400

    added jwt,jwks to create the middleware to decode the token sent from the front end and created the error handlers

--- api/server.js
+++ api/server.js
@@ -1,8 +1,25 @@
 import express from 'express';
 import cors from 'cors';
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
+import axios from 'axios';
 
 const app = express();
 app.use(cors());
+
+const verifyJwt = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://dev-v88tfgqc.us.auth0.com/.well-known/jwks.json',
+  }),
+  audience: 'this is the back end api connection',
+  issuer: 'https://dev-v88tfgqc.us.auth0.com/',
+  algorithms: ['RS256'],
+}).unless({ path: ['/'] });
+
+app.use(verifyJwt);
 
 app.get('/', (req, res) => {
   res.send('Hello from index route');
@@ -10,6 +27,18 @@ app.get('/', (req, res) => {
 
 app.get('/protected', (req, res) => {
   res.send('Hello from protected route');
+});
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  const status = error.status || 500;
+  const message = error.message || 'Internal server error';
+  res.status(status).send(message);
 });
 
 app.listen(4000, () => console.log('Server running on port 4000'));
