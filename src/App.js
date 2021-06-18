fbf8cbdb42c43d49c31c4ecbc9b1683cfb25a306
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-18 02:12:53 -0400

    added the hook useAuth0 and extraced from it the function to login with popup, with redirect and logout, also user information and isAuthenticated method as well

--- src/App.js
+++ src/App.js
@@ -1,56 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+  const { loginWithPopup, loginWithRedirect, logout, user, isAuthenticated } =
+    useAuth0();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <h1>Auth0 authentication</h1>
+      <ul>
+        <li>
+          <button onClick={loginWithPopup}>Login with Popup</button>
+          <button onClick={loginWithRedirect}>Login with Redicrect</button>
+          <button onClick={logout}>Logout</button>
+        </li>
+      </ul>
+      <h3>User is {isAuthenticated ? 'Logged in' : 'Not logged in'}</h3>
+      {isAuthenticated && (
+        <pre style={{ textAlign: 'start' }}>
+          {JSON.stringify(user, null, 2)}
+        </pre>
+      )}
     </div>
   );
 }
