14e52822fd368b9d8ed1f8df17184a2109499ab5
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-18 03:31:10 -0400

    extracting getaccestoken silently from the hook Auth0 to get the token

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import './App.css';
 import { useAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
 
 function App() {
-  const { loginWithPopup, loginWithRedirect, logout, user, isAuthenticated } =
-    useAuth0();
+  const {
+    loginWithPopup,
+    loginWithRedirect,
+    logout,
+    user,
+    isAuthenticated,
+    getAccessTokenSilently,
+  } = useAuth0();
+
+  const callApi = async () => {
+    try {
+      const response = await axios.get('http://localhost:4000');
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // const callProtectedApi = async () => {
+  //   try {
+  //     const response = await axios.get('http://localhost:4000/protected');
+  //     console.log(response.data);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
+  const callProtectedApi = async () => {
+    const token = await getAccessTokenSilently();
+    console.log(token);
+  };
   return (
     <div className="App">
       <h1>Auth0 authentication</h1>
@@ -16,11 +46,15 @@ function App() {
         </li>
       </ul>
       <h3>User is {isAuthenticated ? 'Logged in' : 'Not logged in'}</h3>
-      {isAuthenticated && (
-        <pre style={{ textAlign: 'start' }}>
-          {JSON.stringify(user, null, 2)}
-        </pre>
-      )}
+      <ul>
+        <li>
+          <button onClick={callApi}>Call API</button>
+        </li>
+        <li>
+          <button onClick={callProtectedApi}>Call Proteced API route </button>
+        </li>
+      </ul>
+      <pre style={{ textAlign: 'start' }}>{JSON.stringify(user, null, 2)}</pre>
     </div>
   );
 }
